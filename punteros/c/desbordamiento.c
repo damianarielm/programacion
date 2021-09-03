3b381fb220ee6caabd8821c66c7a651038c8dd57
Damian Ariel Marotte <drd00m@msn.com>
2021-09-03 07:59:35 -0300

    Agrego programas de punteros en C.

---
+++ punteros/c/desbordamiento.c
@@ -0,0 +1,28 @@
+// gcc password.c -fno-stack-protector
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define LEN 10
+
+void main() {
+    char password[LEN];
+    char input[LEN];
+
+    // Generacion de contraseña aleatoria
+    srand(time(NULL));
+    for (int i = 0; i < LEN; i++)
+        password[i] = rand();
+
+    // Lectura de contraseña por teclado
+    printf("Ingrese la contraseña: ");
+    scanf("%s", input);
+
+    // Verificacion de contraseña
+    if (!strncmp(input, password, LEN))
+        printf("Contraseña correcta.\n");
+    else
+        printf("Contraseña incorrecta.\n");
+}
