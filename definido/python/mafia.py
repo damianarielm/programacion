699ef724856cd5967dbc98eb6a97094a9e2b5304
Damian Ariel Marotte <drd00m@msn.com>
2023-02-28 05:08:36 -0300

    Mejoro juego Mafia de Cuba.

--- definido/python/mafia.py
+++ definido/python/mafia.py
@@ -19,31 +19,29 @@ if opcion == 3: agentes -= 1
 if opcion == 4: choferes -= 1
 
 for i in range(2, jugadores + 1):
-    for _ in range(99): print("")
     if diamantes: print(f"Diamantes: {diamantes}.")
     if leales: print(f"Leales: {leales}.")
     if agentes: print(f"Agentes: {agentes}.")
     if choferes: print(f"Choferes: {choferes}.")
 
-    print("0 - Pasar.")
     if diamantes: print("1 - Robar diamantes.")
     if leales: print("2 - Elegir rol leal.")
     if agentes: print("3 - Elegir rol agente.")
     if choferes: print("4 - Elegir rol chofer.")
+    if diamantes + leales + agentes + choferes == 0 or i == jugadores:
+        print("5 - Elegir rol pillo.")
 
     opcion = int(input(f"Jugador {i}, elija una opcion: "))
-
     if opcion == 1: diamantes -= int(input("Ingrese cantidad de diamantes: "))
     if opcion == 2: leales -= 1
     if opcion == 3: agentes -= 1
     if opcion == 4: choferes -= 1
 
-for _ in range(99): print("")
+    print("\n" * 99)
+
 print(f"Diamantes: {diamantes}.")
 print(f"Leales: {leales}.")
 print(f"Agentes: {agentes}.")
 print(f"Choferes: {choferes}.")
 print(f"Comodines: {comodines}.")
 print(f"Comienza el juego!")
-input("Ingrese enter para borrar la pantalla.")
-for _ in range(99): print("")
