1834b4ff63d833d50d23478b0e7cc77098264ed6
Damian Ariel Marotte <drd00m@msn.com>
2023-02-28 04:45:49 -0300

    Mejoro sucesion de Fibonacci.

--- definido/python/fibonacci1.py
+++ definido/python/fibonacci1.py
@@ -1,7 +1,9 @@
+cantidad = int(input("Ingrese la cantidad de numeros: "))
+
 fa = 1
 fb = 1
 
-for i in range(3, 33):
+for i in range(3, cantidad + 3):
     print(fa)
     fi = fa + fb
     fa, fb = fb, fi
--- definido/python/fibonacci2.py
+++ definido/python/fibonacci2.py
@@ -1,4 +1,6 @@
+cantidad = int(input("Ingrese la cantidad de numeros: "))
+
 phi = (1 + 5**0.5) / 2
 
-for n in range(1, 10):
+for n in range(1, cantidad + 1):
     print( (phi**n - (1 - phi)**n) // (5**0.5) )
