349e005d03e40bbde166f24dc5b5135ee61998a8
Damian Ariel Marotte <drd00m@msn.com>
2023-03-05 23:06:18 -0300

    Mejoro algoritmo de ordenamiento Merge Sort.

--- ordenamiento/python/merge.py
+++ ordenamiento/python/merge.py
@@ -1,24 +1,16 @@
-lista = [200, 190, 1200, 1, 2, 4, 55, 1000, 6, 800]
+from combinar import merge # Ejercicio previo
+from permutacion import permutacion # Ejercicio previo
 
+cantidad = int(input("Ingrese la cantidad: "))
+lista = permutacion(cantidad)
 print(f"Lista desordenada: {lista}.")
 
-def merge(lista1, lista2):
-    if not lista1:
-        return lista2
-    elif not lista2:
-        return lista1
-    elif lista1[0] < lista2[0]:
-        return [ lista1[0] ] + merge(lista1[1:], lista2)
-    else:
-        return [ lista2[0] ] + merge(lista1, lista2[1:])
+def merge_sort(lista):
+    if len(lista) <= 1: return lista
 
-def ordenar(lista):
-    if len(lista) <= 1:
-        return lista
-    else:
-        mitad = len(lista) // 2
-        izquierda = lista[:mitad]
-        derecha = lista[mitad:]
-        return merge(ordenar(izquierda), ordenar(derecha))
+    mitad = len(lista) // 2
+    izquierda = lista[:mitad]
+    derecha = lista[mitad:]
+    return merge(merge_sort(izquierda), merge_sort(derecha))
 
-print(f"Lista ordenada: {ordenar(lista)}.")
+print(f"Lista ordenada: {merge_sort(lista)}.")
